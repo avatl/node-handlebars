db5b380ed122c6b3fffdd5a2ebb4923c2f2cd920
Ava Lorusso <ava.tlorusso@gmail.com>
2021-01-19 17:29:39 -0500

    added files

--- seed.sql
+++
@@ -1,6 +0,0 @@
--- Insert rows into table 'TableName'
-INSERT INTO burgers( burger_name,devoured) VALUE ('chicken burger', FALSE);
--- Insert rows into table 'TableName'
-INSERT INTO burgers( burger_name,devoured) VALUE ('Cheese Burger', FALSE);
--- Insert rows into table 'TableName'
-INSERT INTO burgers( burger_name,devoured) VALUE ('Veggie Burger', FALSE);
